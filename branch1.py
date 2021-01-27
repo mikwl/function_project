7338dc0e0576cfa1def5c6019775416ab9c27f66
mparkh <jauntymike@gmail.com>
2021-01-27 20:15:09 +0300

    Added new name function

---
+++ branch1.py
@@ -0,0 +1,13 @@
+def name_user(name):
+    # name = str(input('Enter your name: '))
+    print(f'Hello, {name}.')
+    return
+
+names = ['Tinky-Vinky', 'Dipsey', 'Lala', 'Po']
+
+for i in names:
+    name_user(i)
+
+
+
+
--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print('Hello world')
+# print('Hello world')
+
+def my_func(a, b):
+    summ = a + b
+    print(f'{a} + {b} = {summ}')
+    return summ
+    # РЕТУРН НУЖНО ОНА ЗАПИСЫВАЕТ / ВОЗВРАЩАЕТ НАМ ЗНЧЕНИЕ И ЭТО ЗНАЧЕНИЕ МЫ МОЖЕМ ЗАПИСАТЬ В КАКУЮ-ТО ПЕРЕМЕННУЮ
+
+my_func(4, 5)
+
+my_func(10, 10)
+
+my_func('lala', 'bebebe')
+
+a = my_func(4, 5)
+print(a)
